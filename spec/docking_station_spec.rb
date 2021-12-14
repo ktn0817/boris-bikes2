f892f4a9fa4e8dba1d21299307c982fe2fb9085f
Aadam Ali <ali.aadam@outlook.com>
2021-12-14 15:57:01 +0000

    added test to check if bike docks

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,5 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
@@ -11,4 +12,10 @@ describe DockingStation do
   it { is_expected.to respond_to(:dock).with(1).argument}
 
   it { is_expected.to respond_to :bike }
+
+  it 'docks a bike' do
+    bike = Bike.new
+
+    expect(subject.dock(bike).to eq bike)
+  end
 end
